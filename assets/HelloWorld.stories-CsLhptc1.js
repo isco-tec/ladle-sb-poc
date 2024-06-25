bfada0aebe64ac779a5991200b8c7c379a3236c3
isco-tec <isco-tec@users.noreply.github.com>
2024-06-25 09:50:47 +0000

    deploy: 7b2dbb6362e3ca0f26873bfcda0613c1dff7abe9

--- assets/HelloWorld.stories-CsLhptc1.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Db1ALHkZ.js";const o=()=>e.jsx("div",{children:"Hello World"});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{o as HelloWorldStories};
