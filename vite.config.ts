7b2dbb6362e3ca0f26873bfcda0613c1dff7abe9
isco-tec <oiscovici@gmail.com>
2024-06-25 12:50:07 +0300

    update deployment script and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  // base: '/ladle-sb-poc/',
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
+  return {
+    base: isGithubPages ? '/ladle-sb-poc/' : '/',
+    plugins: [react()],
+  };
 });
