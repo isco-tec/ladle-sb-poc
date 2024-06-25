4d4e2c8e07e0656904cf5fb0ef8c8ed714f18ce7
isco-tec <oiscovici@gmail.com>
2024-06-25 12:43:44 +0300

    testing without base config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ladle-sb-poc/',
+  // base: '/ladle-sb-poc/',
   plugins: [react()],
 });
